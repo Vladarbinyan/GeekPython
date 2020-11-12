e432de76aae1a0e24fdb88d9318e3d11ce63685e
Vladarbinyan <clab@mail.ru>
2020-11-13 03:11:05 +0700

    Выполнил пятое задание.

--- Lesson03/HomeWork3.5.py
+++ Lesson03/HomeWork3.5.py
@@ -5,4 +5,46 @@
 программы завершается. Если специальный символ введен после нескольких чисел, то вначале нужно добавить сумму этих
 чисел к полученной ранее сумме и после этого завершить программу.
 """
+total = 0
 
+
+def adder(*numbers):
+    global total
+    for el in numbers:
+        if el == 'q':
+            return 'stop'
+        current = str_float(el)
+        if current is not None:
+            total += current
+    return
+
+
+def is_empty(string):
+    if string.isspace() or not bool(string):
+        return True
+
+
+def str_float(string):
+    """
+    :param string:
+    :return: float или None
+    """
+    try:
+        return float(string)
+    except ValueError:
+        return
+
+
+while True:
+    input_string = input(
+        'Введите строку чисел разделенных пробелами, для завершения работы вместо числа введите символ \"q" \n'
+    )
+    if is_empty(input_string):
+        print('Пустая строка')
+        continue
+    else:
+        numbers = input_string.split()
+        result_code = adder(*numbers)
+        print(total)
+        if result_code == 'stop':
+            break
