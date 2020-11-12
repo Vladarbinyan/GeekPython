5affb6a41caada742ddd54d93614488519c5d15f
Vladarbinyan <clab@mail.ru>
2020-11-13 02:21:30 +0700

    Выполнил шестое задание.

--- Lesson03/HomeWork3.6.py
+++ Lesson03/HomeWork3.6.py
@@ -6,3 +6,25 @@
 написанную ранее функцию int_func().
 """
 
+
+def int_func(word: str):
+    return word.capitalize()
+
+
+def is_empty(string):
+    if string.isspace() or not bool(string):
+        return True
+
+
+while True:
+    input_string = input('Введите слово или строку слов разделенных пробелами \n')
+    if is_empty(input_string):
+        print('Пустая строка')
+        continue
+    else:
+        words = input_string.split()
+        capitalized_words = []
+        for word in words:
+            capitalized_words.append(int_func(word))
+        print(capitalized_words)
+        break
