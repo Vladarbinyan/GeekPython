3225f73fcdb789432c7e5bc55e3a324db9dd4a5b
Vladarbinyan <clab@mail.ru>
2020-11-21 00:43:02 +0700

    Третье задание седьмого урока.

--- Lesson07/HW7.1.py
+++ Lesson07/HW7.1.py
@@ -16,8 +16,7 @@ class Matrix:
         self._cols = len(matrix[0])
         for row in matrix:
             if len(row) != self._cols:
-                print('матрица должна быть прямоугольной')
-                raise ValueError  # Возможность создавать новые матрицы через try
+                raise ValueError(f'Ошибка создания, матрица должна быть прямоугольной')  # Возможность создавать новые матрицы через try
         self.__matrix = matrix
 
     def __str__(self):
@@ -29,38 +28,32 @@ class Matrix:
         return matrix_print
 
     def __add__(self, other: "Matrix"):
-        if not isinstance(other, Matrix):
-            print('Второй объект не матрица')
-            raise ValueError
-        if self._rows != other._rows or self._cols != other._cols:
-            print('Нельзя складывать матрицы разной размерности')
-            raise ValueError
-        result_matrix = []
-        for row in range(self._rows):
-            new_row = []
-            for el in range(self._cols):
-                new_row.append(self.__matrix[row][el] + other.__matrix[row][el])
-            result_matrix.append(new_row)
-        return Matrix(result_matrix)
+        if not isinstance(other, Matrix) or self._rows != other._rows or self._cols != other._cols:
+            print('Ошибка сложения матриц!')
+            raise ValueError('Можно складывать только матрицы. Матрицы должны быть одинаковой размерности')
+        else:
+            result_matrix = []
+            for row in range(self._rows):
+                new_row = []
+                for el in range(self._cols):
+                    new_row.append(self.__matrix[row][el] + other.__matrix[row][el])
+                result_matrix.append(new_row)
+            return Matrix(result_matrix)
 
-
-m0 = Matrix([[31, 22], [37, 43], [51, 86]])
-m1 = Matrix([[3, 5, 32], [2, 4, 6], [-1, 64, -8]])
-m2 = Matrix([[3, 5, 32], [2, 4, 6], [-1, 64, -8]])
-m3 = Matrix([[3, 5, 8, 3], [8, 3, 7, 1]])
-
-print(m1)
-print(m2)
-print(m3)
 
 try:
-    m4 = m0 + m1
-except ValueError:
-    print('Можно складывать только матрицы, которые обладают одинаковой размерностью')
+    m0 = Matrix([[31, 22], [37, 43], [51, 86]])
+    m1 = Matrix([[3, 5, 32], [2, 4, 6], [-1, 64, -8]])
+    m2 = Matrix([[3, 5, 32], [2, 4, 6], [-1, 64, -8]])
+    # m3 = Matrix([[3, 5, 8, 3], [8, 3, 7, 1, 4]]) # пример генерации ошибки создания
 
-try:
+    print(m1)
+    print(m2)
+
+    # m4 = m0 + m1 # пример генерации ошибки сложения
+
     m4 = m1 + m2
-except ValueError:
-    print('Проверьте операнды')
 
-print(m4)
+    print(m4)
+except ValueError as error:
+    print(error)
