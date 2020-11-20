f8dde13e04d934297c45b0e1e1dc68b8dc174834
Vladarbinyan <clab@mail.ru>
2020-11-21 01:33:14 +0700

    Фиксы ДЗ седьмого урока.

--- Lesson07/HW7.3.py
+++ Lesson07/HW7.3.py
@@ -20,45 +20,29 @@ make_order(), принимающий экземпляр класса и коли
 
 class Cell:
     def __init__(self, cell_size: int):
-        if not isinstance(cell_size, int) or cell_size < 1:
-            print('Задайте размер клетки целым числом больше нуля')
-            raise ValueError  # буду генерировать исключение, но обрабатывать не буду. Для простоты выведу ошибки прямо
-            # в методе класса.
+        assert cell_size > 0, 'Задайте размер клетки целым числом больше нуля'
         self.__cell_size = cell_size
 
     def __str__(self):
         return f'Клетка размером {self.__cell_size}'
 
     def __add__(self, other):
-        if not isinstance(other, Cell):
-            print('Второй объект не клетка')
-            raise ValueError
-        else:
-            return Cell(self.__cell_size + other.__cell_size)
+        self.validate_item(other)
+        return Cell(self.__cell_size + other.__cell_size)
 
     def __sub__(self, other):
-        if not isinstance(other, Cell):
-            print('Второй объект не клетка')
-            raise ValueError
-        elif self.__cell_size <= other.__cell_size:
-            print('Результат вычитания клеток должен быть больше нуля')
-            return None
-        else:
-            return Cell(self.__cell_size - other.__cell_size)
+        self.validate_item(other)
+        value = self.__cell_size - other.__cell_size
+        assert value > 0, 'Результат вычитания клеток должен быть больше нуля'
+        return Cell(value)
 
     def __mul__(self, other):
-        if not isinstance(other, Cell):
-            print('Второй объект не клетка')
-            raise ValueError
-        else:
-            return Cell(self.__cell_size * other.__cell_size)
+        self.validate_item(other)
+        return Cell(self.__cell_size * other.__cell_size)
 
     def __truediv__(self, other):
-        if not isinstance(other, Cell):
-            print('Второй объект не клетка')
-            raise ValueError
-        else:
-            return Cell(self.__cell_size // other.__cell_size)
+        self.validate_item(other)
+        return Cell(self.__cell_size // other.__cell_size)
 
     def make_order(self, cols):
         if not isinstance(cols, int) or cols < 1:
@@ -71,20 +55,20 @@ class Cell:
             ordered_cell += '*' * (self.__cell_size % cols) + '\n'
             return ordered_cell
 
+    def validate_item(self, other):
+        assert isinstance(other, self.__class__), 'Операции допустимы только между клетками'
+
 
 cell01 = Cell(14)
 print(cell01.make_order(5))
 
 cell02 = Cell(7)
-cell03 = cell01+cell02
+cell03 = cell01 + cell02
 cell04 = cell02 - cell01
 cell05 = cell03 - cell02
-cell06 = cell03/cell02
+cell06 = cell03 / cell02
 
-
-print(f'Умножение клеток: {cell01} * {cell02} = {cell01*cell02}')
-print(f'Деление клеток: {cell01} / {cell02} = {cell01/cell02}')
-print(f'Сложение клеток: {cell01} + {cell02} = {cell01+cell02}')
-print(f'Вычитание клеток: {cell01} - {cell02} = {cell01-cell02}')
-
-
+print(f'Умножение клеток: {cell01} * {cell02} = {cell01 * cell02}')
+print(f'Деление клеток: {cell01} / {cell02} = {cell01 / cell02}')
+print(f'Сложение клеток: {cell01} + {cell02} = {cell01 + cell02}')
+print(f'Вычитание клеток: {cell01} - {cell02} = {cell01 - cell02}')
